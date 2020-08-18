ad248223acc520f60bf63ca69b0d4f569b6e316d
Megan Comeau <mlcomeau99@gmail.com>
2020-08-18 18:09:59 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,6 +1,6 @@
 export function managePresents(state, action){
   switch (action.type) {
-    case 'INCREASE_COUNT':
+    case 'INCREASE':
       return {count: state.count + 1}
     default:
       return state;
